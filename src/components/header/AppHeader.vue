a60b22ddb29098d1324e5fe97751a3aad4fef1d5
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-01-09 15:20:52 +0100

    Added other general styles , more specific styles for the header and changed the icon for the project

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -2,8 +2,16 @@
 </script>
 
 <template>
-    <header>
+    <header class="container">
+        <nav class="d-flex justify-between align-center">
+            <div class="logo-container">
+                <img src="../../assets/img/dc-logo.png" alt="">
+            </div>
 
+            <ul class="d-flex">
+                <li><a href="#">susi</a></li>
+            </ul>
+        </nav>
     </header>
 </template>
 
