2a6147d7eb776544f34251d0daf076e17b36fd94
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-01-09 14:51:24 +0100

    Added the main files for the project

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <script>
 import AppHeader from './components/header/AppHeader.vue';
+import FooterNavbar from './components/footer/FooterNavbar.vue';
+import FooterSocialLinks from './components/footer/FooterSocialLinks.vue';
 
 
 
 export default {
   components: {
     AppHeader,
+    FooterNavbar,
+    FooterSocialLinks,
   }
 }
 </script>
 
 <template>
   <AppHeader />
+  <footer>
+    <FooterNavbar />
+    <FooterSocialLinks />
+  </footer>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
