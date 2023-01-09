f52ee5ca546c6a80ab76575b644f07f99f7585a3
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-01-09 15:59:01 +0100

    Modified the header

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -2,19 +2,45 @@
 </script>
 
 <template>
-    <header class="container">
-        <nav class="d-flex justify-between align-center">
+    <header>
+        <nav class="d-flex justify-between align-center container">
             <div class="logo-container">
                 <img src="../../assets/img/dc-logo.png" alt="">
             </div>
 
             <ul class="d-flex">
-                <li><a href="#">susi</a></li>
+                <li class="p-relative">
+                    <a href="#">sha</a>
+                    <div class="decoration p-absolute"></div>
+                </li>
+
+                <li class="p-relative active">
+                    <a href="#">sha</a>
+                    <div class="decoration p-absolute"></div>
+                </li>
+
+                <li class="p-relative">
+                    <a href="#">sha</a>
+                    <div class="decoration p-absolute"></div>
+                </li>
             </ul>
         </nav>
     </header>
 </template>
 
 <style scoped lang="scss">
+ul li {
+    margin-right: 2.3rem;
 
+    &.active {
+        .decoration {
+            height: 0.3rem;
+            width: 100%;
+
+            &.p-absolute {
+                top: 55px;
+            }
+        }
+    }
+}
 </style>
--- src/components/main/MainContent.vue
+++ src/components/main/MainContent.vue
@@ -2,6 +2,9 @@
 </script>
 
 <template>
+    <section id="content" class="container">
+        <h2>Content goes here</h2>
+    </section>
 </template>
 
 <style scoped lang="scss">
