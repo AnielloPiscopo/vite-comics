7e377add23593e001f4f13624260fcd5728d8b9b
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-01-09 21:56:18 +0100

    Added the first informations in the main nav

--- src/components/main/MainNavbar.vue
+++ src/components/main/MainNavbar.vue
@@ -2,8 +2,15 @@
 </script>
 
 <template>
+    <nav class="container"></nav>
 </template>
 
 <style scoped lang="scss">
 @use '../../styles/partials/variables' as *;
+
+
+
+nav {
+    background-color: $complementary--color;
+}
 </style>
