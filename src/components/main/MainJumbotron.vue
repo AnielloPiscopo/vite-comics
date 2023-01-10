337ab341fc1a807f557fe1ed3f45f80be0a02f67
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-01-10 16:21:47 +0100

    Added the props

--- src/components/main/MainJumbotron.vue
+++ src/components/main/MainJumbotron.vue
@@ -1,12 +1,31 @@
 <script>
+import { stringifyExpression } from '@vue/compiler-core';
+import SingleComic from './SingleComic.vue';
+console.log(json)
+import json from './../../assets/json/dc-comics.json';
+
 export default {
     name: 'MainJumbotron',
+
+    components: {
+        SingleComic,
+    },
+
+    data() {
+        return {
+            json,
+        }
+    },
 }
 </script>
 
 <template>
     <section id="jumbotron" class="container">
         <h3 class="title">--&#62; Content goes here &#60;--</h3>
+        <section id="comics-container">
+            <SingleComic v-for="comicInfo in json" :thumb="comicInfo.thumb" :price="comicInfo.price"
+                :series="comicInfo.series" :type="json.type" />
+        </section>
     </section>
 </template>
 
---
+++ src/components/main/SingleComic.vue
@@ -0,0 +1,20 @@
+
+<script>
+export default {
+    props: {
+        thumb: String,
+        price: Number,
+        series: String,
+        type: String,
+    }
+}
+
+</script>
+
+<template>
+    <article class="comic">{{ price }}</article>
+</template>
+
+<style lang="">
+    
+</style>
